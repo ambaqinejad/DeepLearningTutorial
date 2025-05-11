02f814082841a07c289afba7996925ee4115afe6
ambaqinejad <ambaqinejad@gmail.com>
2025-05-11 13:19:57 +0330

    wip

--- 01-PyTorch Neural Network Classification/1-classification.py
+++ 01-PyTorch Neural Network Classification/1-classification.py
@@ -4,6 +4,8 @@ import torch
 from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
 from torch import nn
+from helper_functions import plot_predictions, plot_decision_boundary
+
 
 def accuracy_fn(y_true, y_pred):
     correct = torch.eq(y_true, y_pred).sum().item()
@@ -48,21 +50,55 @@ class CircleModel1(nn.Module):
     def forward(self, X):
         return self.layer2(self.layer1(X))
 
-model = CircleModel1().to(device)
+class CircleModel2(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.layer1 = nn.Linear(in_features=2, out_features=10)
+        self.layer2 = nn.Linear(in_features=10, out_features=10)
+        self.layer3 = nn.Linear(in_features=10, out_features=1)
+
+    def forward(self, X):
+        return self.layer3(self.layer2(self.layer1(X)))
+
+model = CircleModel2().to(device)
 model1 = nn.Sequential(
     nn.Linear(in_features=2, out_features=5),
     nn.Linear(in_features=5, out_features=1)
 )
 
 loss_fn = nn.BCEWithLogitsLoss()
-optimizer = torch.optim.SGD(params=model.parameters(), lr=.1)
+optimizer = torch.optim.SGD(params=model.parameters(), lr=0.1)
 print(model)
 
 torch.manual_seed(42)
-epochs = 100
+epochs = 1000
 X_train, y_train = X_train.to(device), y_train.to(device)
 X_test, y_test = X_test.to(device), y_test.to(device)
 for epoch in range(epochs):
     model.train()
-    y = model(X_train)
-    y_pred = torch.round()
+    y = model(X_train).squeeze()
+    y_pred = torch.round(torch.sigmoid(y))
+    loss = loss_fn(y_pred, y_train)
+    acc = accuracy_fn(y_train, y_pred)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    model.eval()
+    with torch.inference_mode():
+        test = model(X_test).squeeze()
+        test_pred = torch.round(torch.sigmoid(test))
+        test_loss = loss_fn(test_pred, y_test)
+        test_acc = accuracy_fn(y_test, test_pred)
+
+    if (epoch % 10) == 0:
+        print(f"Epoch: {epoch} | Loss: {loss:.5f}, Accuracy: {acc:.2f}% | Test loss: {test_loss:.5f}, Test acc: {test_acc:.2f}%")
+
+# Plot decision boundaries for training and test sets
+plt.figure(figsize=(12, 6))
+plt.subplot(1, 2, 1)
+plt.title("Train")
+plot_decision_boundary(model, X_train, y_train)
+plt.subplot(1, 2, 2)
+plt.title("Test")
+plot_decision_boundary(model, X_test, y_test)
+plt.show()
