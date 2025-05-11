2c05b10ae9b920d0f97c8df7db89f7c2a20854cd
ambaqinejad <ambaqinejad@gmail.com>
2025-05-11 10:20:19 +0330

    wip

--- 01-PyTorch Neural Network Classification/1-classification.py
+++ 01-PyTorch Neural Network Classification/1-classification.py
@@ -1,9 +1,17 @@
 from sklearn.datasets import make_circles
 import pandas as pd
+import torch
+from matplotlib import pyplot as plt
+from sklearn.model_selection import train_test_split
+from torch import nn
+
+def accuracy_fn(y_true, y_pred):
+    correct = torch.eq(y_true, y_pred).sum().item()
+    acc = (correct/len(y_pred)) * 100
+    return acc
 
 n_samples = 1000
-
-X, y = make_circles(n_samples, noise=.03, random_state=42)
+X, y = make_circles(n_samples=n_samples, noise=.03, random_state=42)
 
 print(X[:5])
 print(y[:5])
@@ -11,8 +19,50 @@ print(y[:5])
 circles = pd.DataFrame({
     "X1": X[:, 0],
     "X2": X[:, 1],
-    "y": y
+    "label": y
 })
 
-print(circles.head(10))
-print(circles.y.value_counts())
+print(circles.head())
+print(circles.label.value_counts())
+plt.scatter(x=X[:, 0],
+            y=X[:, 1],
+            c=y)
+# plt.show()
+
+X = torch.from_numpy(X).type(torch.float)
+y = torch.from_numpy(y).type(torch.float)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.2, random_state=42)
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+print(X[:5])
+print(y[:5])
+
+class CircleModel1(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.layer1 = nn.Linear(in_features=2, out_features=5)
+        self.layer2 = nn.Linear(in_features=5, out_features=1)
+
+    def forward(self, X):
+        return self.layer2(self.layer1(X))
+
+model = CircleModel1().to(device)
+model1 = nn.Sequential(
+    nn.Linear(in_features=2, out_features=5),
+    nn.Linear(in_features=5, out_features=1)
+)
+
+loss_fn = nn.BCEWithLogitsLoss()
+optimizer = torch.optim.SGD(params=model.parameters(), lr=.1)
+print(model)
+
+torch.manual_seed(42)
+epochs = 100
+X_train, y_train = X_train.to(device), y_train.to(device)
+X_test, y_test = X_test.to(device), y_test.to(device)
+for epoch in range(epochs):
+    model.train()
+    y = model(X_train)
+    y_pred = torch.round()
